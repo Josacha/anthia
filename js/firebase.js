5306630bc48f2c2ea343569e8518276f50e14c0d
Josacha <120397811+Josacha@users.noreply.github.com>
2026-01-19 16:22:18 -0600

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,34 @@
+// js/firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+// Analytics es opcional
+import { getAnalytics, isSupported } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCpLE36u3xpQUZiUUBGHZTgcuTTAMiHybY",
+  authDomain: "anthia-salon.firebaseapp.com",
+  projectId: "anthia-salon",
+  storageBucket: "anthia-salon.firebasestorage.app",
+  messagingSenderId: "1013724768884",
+  appId: "1:1013724768884:web:e10c0cc47ac854704c3f21",
+  measurementId: "G-X53ET2LTGM"
+};
+
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+
+// Servicios principales
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+// Analytics (solo si el navegador lo soporta)
+let analytics;
+isSupported().then((yes) => {
+  if (yes) {
+    analytics = getAnalytics(app);
+  }
+});
+
+export { analytics };
 
