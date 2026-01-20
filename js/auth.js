39536b11470e4afb5adaafb54f8ddc4156061b31
Josacha <120397811+Josacha@users.noreply.github.com>
2026-01-19 18:15:35 -0600

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,28 @@
+import { auth } from './firebase.js';
+import { signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+const formLogin = document.getElementById('formLogin');
+const errorMsg = document.getElementById('errorMsg');
+
+formLogin.addEventListener('submit', async e => {
+  e.preventDefault();
+  const email = document.getElementById('loginEmail').value.trim();
+  const password = document.getElementById('loginPassword').value;
+
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+    // Redirigir a dashboard
+    window.location.href = 'dashboard.html';
+  } catch (error) {
+    console.error(error);
+    errorMsg.textContent = 'Correo o contraseña incorrectos.';
+  }
+});
+
+// Detectar si ya hay sesión activa
+onAuthStateChanged(auth, user => {
+  if (user) {
+    window.location.href = 'dashboard.html';
+  }
+});
 
