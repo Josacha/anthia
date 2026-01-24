35079ef218c9ab89b31cee4283de9afc8d5b3450
Josacha <120397811+Josacha@users.noreply.github.com>
2026-01-24 15:58:06 -0600

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -15,7 +15,7 @@ onAuthStateChanged(auth, (user) => {
 
   // ❌ NO logueado y NO está en login → mandar a login
   if (!user && !esLogin) {
-    window.location.href = "/anthia/admin/login.html";
+    window.location.href = "login.html";
   }
 
   // ✅ Logueado y está en login → mandar a dashboard
